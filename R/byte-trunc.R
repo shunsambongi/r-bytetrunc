31258536fcd76e5fbc055b8ac66a093b1af15194
Shun Sambongi <shunsambongi@gmail.com>
2022-06-23 10:43:07 -0600

    Remove stringi dependency

--- R/byte-trunc.R
+++ R/byte-trunc.R
@@ -10,8 +10,7 @@
 #' byte_trunc(y, 10)
 byte_trunc <- function(string, size) {
   not_na <- !is.na(string)
-  utf8 <- stringi::stri_conv(string[not_na], to = "UTF-8")
-  string[not_na] <- byte_trunc_(utf8, size)
+  string[not_na] <- byte_trunc_(enc2utf8(string[not_na]), size)
   Encoding(string) <- "UTF-8"
   string
 }
